ee3d820bbb562c03c2e83691da8e2daa37e575b7
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-01 23:05:13 +0530

    Add files via upload

---
+++ CPP-Programs/Patterns/Pattern13.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n) {
+    int num=1;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout<<num<<" ";
+            num=num+1;
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    int n, t;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        cin>>n;
+        print(n);
+    }
+    return 0;
+}
---
+++ CPP-Programs/Patterns/Pattern21.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i==0 || j==0 || i==n-1 || j==n-1) {
+                cout<<"*";
+            } else {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    int n, t;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        cin>>n;
+        print(n);
+    }
+    return 0;
+}

983c8908a9744f7b3d90efcbb91e5c5cff124151
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-04 21:19:30 +0530

    Bubble sort

--- CPP-Programs/Sorting/bubbleSort.cpp
+++ CPP-Programs/Sorting/bubbleSort.cpp
@@ -8,10 +8,15 @@ void display(int arr[], int n) {
 }
 void bubble_sort(int arr[], int n) {
     for(int i=0; i<n-1; i++) {
+        int didSwap = 0;
         for(int j=0; j<n-i-1; j++) {
             if(arr[j]>arr[j+1]) { 
                 swap(arr[j], arr[j+1]);
+                didSwap=1;
             }       
+        }
+        if(didSwap==0) {
+            break;
         }
     }
 }

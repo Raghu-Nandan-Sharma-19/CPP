d38a6b4bf21a11a7734f7c177ad6ec14262311dc
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-07 18:32:43 +0530

    Left rotate array by one place

---
+++ CPP-Programs/Arrays/rotateArrayByOnePlace.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(vector<int> arr, int n) {
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    } cout<<endl;
+}
+vector<int> leftRotate(vector<int> &arr, int n) {
+    int temp = arr[0];
+    for(int i=1; i<n; i++) {
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+    return {arr};
+}
+int main() {
+    int size;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>size;
+    vector<int> arr;
+    cout<<"Enter "<<size<<" elements : "<<endl;
+    for(int i=0; i<size; i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    cout<<"The array after left rotating it by one place is : "<<endl;
+    leftRotate(arr, size);
+    display(arr, size);
+    return 0;
+}

d03b72cf051d07a6ba03464fa872bfa1be8a9492
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-07 20:16:03 +0530

    Optimized the code

--- CPP-Programs/Arrays/rotateByDPlaces.cpp
+++ CPP-Programs/Arrays/rotateByDPlaces.cpp
@@ -7,6 +7,18 @@ void display(vector<int> arr, int n) {
 }
 vector<int> rotate(vector<int> &arr, int d) {
     d%=arr.size();
+    // vector<int> temp;
+    // for(int i=0; i<d; i++) {
+    //     temp.push_back(arr[i]);
+    // }
+    // for(int i=d; i<arr.size(); i++) {
+    //     arr[i-d] = arr[i];
+    // }
+    // for(int i=arr.size()-d; i<arr.size(); i++) {
+    //     arr[i] = temp[i-(arr.size()-d)];
+    // }
+    
+    // Optimal code below
     reverse(arr.begin(), arr.end());
     reverse(arr.begin(), arr.begin()+d+1);
     reverse(arr.begin()+d+1, arr.end());

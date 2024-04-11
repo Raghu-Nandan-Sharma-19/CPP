62d33da32c75f9d8f756f8cf21774924041330eb
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-11 19:32:20 +0530

    longest Subarray With Sum K Positives And Negatives

---
+++ CPP-Programs/Arrays/longestSubarrayWithSumKPositivesAndNegatives.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getLongestSubarray(vector<int>& a, long long k) {
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for(int i=0; i<a.size(); i++) {
+        sum+=a[i];
+        if(sum==k) {
+            maxLen = max(maxLen, i+1);
+        }
+        long long rem = sum-k;
+        if(preSumMap.find(rem)!=preSumMap.end()) {
+            int len = i-preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(preSumMap.find(sum)==preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
+int main() {
+    int size;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>size;
+    vector<int> arr;
+    cout<<"Enter "<<size<<" elements : "<<endl;
+    for(int i=0; i<size; i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    int k;
+    cout<<"Enter k : "<<endl;
+    cin>>k;
+    cout<<"The longest subarray with sum k(positives and negatives) is : "<<getLongestSubarray(arr, k)<<endl;
+    return 0;
+}

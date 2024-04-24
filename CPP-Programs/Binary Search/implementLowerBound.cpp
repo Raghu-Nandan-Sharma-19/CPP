dfff9f80ba24752c3445b8de9b18a35852cd54c1
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-04-24 13:06:22 +0530

    Implement lower bound

---
+++ CPP-Programs/Binary Search/implementLowerBound.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerBound(vector<int> arr, int n, int x) {
+	int low = 0, high = n-1; // low and high are the indices of the array
+	int ans = n; // ans is the smallest index of element which is >= x
+	while(low <= high) { // if low is greater than high, then the element is not present in the array
+		int mid = (low+high)/2; // calculating the mid index
+		if(arr[mid] >= x) { // if the element at mid index is greater than or equal to the target element
+			ans = mid; // then update the ans to mid index
+			high = mid-1; // then the target element will be present in the left half of the array
+		} else { // if the element at mid index is less than the target element
+			low = mid+1; // then the target element will be present in the right half of the array
+		}
+	}
+	return ans; // return the smallest index of element which is >= x
+}
+int main() {
+    int size;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>size;
+    vector<int> arr;
+    cout<<"Enter "<<size<<" elements : "<<endl;
+    for(int i=0; i<size; i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    int target;
+    cout<<"Enter target number : "<<endl;
+    cin>>target;
+    cout<<"The smallest index of element which is >= "<<target<<" is : "<<lowerBound(arr, size, target)<<endl;
+    return 0;
+}

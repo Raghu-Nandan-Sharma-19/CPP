941dd04c99e30d4dcd1ca3fe87a142ae6fca2e03
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-05-25 20:40:25 +0530

    Count the number of setbits

---
+++ CPP-Programs/Bit Manipulation/countNoOfSetBits.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countSetBits(int n) {
+    int cnt = 0;
+    while(n != 0) {
+        n = n & (n-1);
+        cnt++;
+    }
+    return cnt;
+}
+int main() {
+    int n = 84;
+    cout<<"The number of setbits in "<<n<<" is : "<<countSetBits(n)<<endl;
+    return 0;
+}

afe218b68ee9ef24e786db6127605dc5430b97ad
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-23 17:33:19 +0530

    Number of substrings with all characters

--- CPP-Programs/Sliding Window And Two Pointer/numberOfSubstringsContainingAllCharacters.cpp
+++ CPP-Programs/Sliding Window And Two Pointer/numberOfSubstringsContainingAllCharacters.cpp
@@ -1,13 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-// //  brute force approach
+// // //  brute force approach
+// int numberOfSubstrings(string s) {
+//     int cnt = 0;
+//     for(int i = 0; i < s.size(); i++) {
+//         vector<int> hash(3, 0);
+//         for(int j = i; j < s.size(); j++) {
+//             hash[s[j] - 'a'] = 1;
+//             if(hash[0] + hash[1] + hash[2] == 3) cnt = cnt + 1; 
+//         }
+//     }
+//     return cnt;
+// }
+// // optimal approach
 int numberOfSubstrings(string s) {
     int cnt = 0;
+    vector<int> lastSeen(3, -1);
     for(int i = 0; i < s.size(); i++) {
-        vector<int> hash(3, 0);
-        for(int j = i; j < s.size(); j++) {
-            hash[s[j] - 'a'] = 1;
-            if(hash[0] + hash[1] + hash[2] == 3) cnt = cnt + 1; 
+        lastSeen[s[i] - 'a'] = i;
+        if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+            cnt = cnt + (1 + min(lastSeen[0], min(lastSeen[1], lastSeen[2])));
         }
     }
     return cnt;

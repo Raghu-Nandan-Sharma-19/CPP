c9ebbc853ffede67e78fae6a83082ce09fbb26a5
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-24 15:04:39 +0530

    Longest substring with atmost k distinct characters

--- CPP-Programs/Sliding Window And Two Pointer/longestSubstringWithAtmostKDistinctCharacters.cpp
+++ CPP-Programs/Sliding Window And Two Pointer/longestSubstringWithAtmostKDistinctCharacters.cpp
@@ -1,15 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+// // Brute force approach
+// int kDistinctChars(int k, string &str) {
+//     int maxLen = 0;
+//     map<char, int> mpp;
+//     for(int i = 0; i < str.size(); i++) {
+//         mpp.clear();
+//         for(int j = i; j < str.size(); j++) {
+//             mpp[str[j]]++;
+//             if(mpp.size() <= k) maxLen = max(maxLen, j - i + 1);
+//             else break;
+//         }
+//     }
+//     return maxLen;
+// }
+
+// // Optimal approach
 int kDistinctChars(int k, string &str) {
-    int maxLen = 0;
+    int maxLen = 0, l = 0, r  = 0;
     map<char, int> mpp;
-    for(int i = 0; i < str.size(); i++) {
-        mpp.clear();
-        for(int j = i; j < str.size(); j++) {
-            mpp[str[j]]++;
-            if(mpp.size() <= k) maxLen = max(maxLen, j - i + 1);
-            else break;
+    while(r < str.size()) {
+        mpp[str[r]]++;
+        if (mpp.size() <= k) {
+          maxLen = max(maxLen, r - l + 1);
         }
+        else {
+            mpp[str[l]]--;
+            if (mpp[str[l]] == 0) {
+                mpp.erase(str[l]);
+            }
+            l++;
+        }
+        
+        r++;
     }
     return maxLen;
 }

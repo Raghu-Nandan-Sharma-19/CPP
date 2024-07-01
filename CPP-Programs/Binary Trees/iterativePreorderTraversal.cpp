14d4ee739f6036afdca6732918af02b9fd293998
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-01 22:28:06 +0530

    Iterative preorder traversal

---
+++ CPP-Programs/Binary Trees/iterativePreorderTraversal.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int value) {
+        data = value;
+        left = right = NULL;
+    }
+};
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    if(root == NULL) return ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        int size = q.size();
+        vector<int> level;
+        for(int i = 0; i < size; i++) {
+            TreeNode *node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    vector<vector<int>> res = levelOrder(root);
+    for(auto level : res) {
+        for(auto ele : level) {
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

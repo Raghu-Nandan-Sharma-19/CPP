e487297685d6cc3a358b689e940a1083ba72a51d
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-28 20:40:48 +0530

    Variables and data types

---
+++ VariablesAndDataTypes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // variable declare
+    int a; // integer - 4 bytes
+    a = 10; // variable define
+    cout<<a<<endl;
+
+    // character - 1 byte
+    char c = 'a';
+    cout<<c<<endl;
+
+    // float - 4 byte
+    float f = 2.31;
+    cout<<f<<endl;
+
+    // double - 8 byte
+    double d = 3.121;
+    cout<<d<<endl;
+
+    // boolean - 1 byte
+    bool b = true;
+    cout<<b<<endl;
+}

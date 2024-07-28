635c2ce3c124c8fd7900c699d475e576b94d8c67
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-28 20:57:58 +0530

    Subtraction

---
+++ Homework/Subtraction.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter a : ";
+    cin>>a;
+    cout<<"Enter b : ";
+    cin>>b;
+    cout<<"After doing a - b answer is : "<<a-b<<endl;
+}

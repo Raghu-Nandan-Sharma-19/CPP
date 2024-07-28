905d9352aa9b7751c953635ef983c686a34edda0
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-28 20:53:09 +0530

    Product of two numbers

---
+++ Homework/ProductOfTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int number1, number2;
+    cout<<"Enter first number : "<<endl;
+    cin>>number1;
+    cout<<"Enter second number : "<<endl;
+    cin>>number2;
+    cout<<"Product of "<<number1<<" and "<<number2<<"is : "<<number1*number2<<endl;
+    return 0;
+}

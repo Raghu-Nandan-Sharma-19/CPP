3e174e6330beadfbce8c3f04bb6469d7cd27ec61
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-29 11:34:28 +0530

    Taking user input

---
+++ TakingUserInput.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
+    cout<<"Sum of numbers is : "<<a+b<<endl;
+    return 0;
+}

d10b317fa74cbf597fe67982e1955ebfe78a4d2f
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-29 13:03:22 +0530

    Check even or odd

---
+++ Problems/even_or_odd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter your number : ";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+    return 0;
+}

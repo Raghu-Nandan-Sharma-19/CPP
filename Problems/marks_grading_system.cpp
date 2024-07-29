3bbb938fa501895bd7110aa7097c8644e8c03556
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-29 12:08:47 +0530

    Marks grading system

---
+++ Problems/marks_grading_system.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int marks;
+    cin>>marks;
+    if(marks > 33)
+    {
+        cout<<"Pass";
+    }
+    else
+    {
+        cout<<"fail";
+    }
+    return 0;
+}

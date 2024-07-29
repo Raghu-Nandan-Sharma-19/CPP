a864424e1b92111159d3c85ff4cf9c47dc0d10aa
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-29 12:36:46 +0530

    Check if A is greater than B

---
+++ Problems/check_if_a_greater_than_b.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+    cout<<"Enter your first number : ";
+    cin>>a;
+    cout<<"Enter your second number : ";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    return 0;
+}

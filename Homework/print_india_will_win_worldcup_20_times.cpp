b819f30b57f5e40058e5adff1ccd7ff976b80d98
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-30 02:31:04 +0530

    Print India will win world cup 20 times

---
+++ Homework/print_india_will_win_worldcup_20_times.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int count=1; count<=20; count++) {
+        cout<<count<<". India will win world cup 2027"<<endl;
+    }
+    return 0;
+}

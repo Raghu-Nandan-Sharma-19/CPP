816d5257579a3984d592047be34877a309404672
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-29 12:00:24 +0530

    If else

---
+++ IfElse.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int package;
+    cout<<"Enter package : ";
+    cin>>package;
+    if(package>10)
+    {
+        cout<<"Accepted";
+    }
+    else
+    {
+        cout<<"Rejected";
+    }
+    return 0;
+}

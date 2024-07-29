027c44aaf621845de696beab353ea71a083fb704
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-30 01:54:55 +0530

    Print n natural numbers

---
+++ Problems/print_n_natural_numbers.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int count=1; count<=n; count++) {
+        cout<<count<<endl;
+    }
+    return 0;
+}

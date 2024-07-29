1bf70789e2f3cec21358430908f2c0bab09efed7
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-30 02:19:13 +0530

    Print adult or teenager

---
+++ Homework/adult_or_teenager.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int age;
+    cout<<"Enter age : ";
+    cin>>age;
+    if(age>18) {
+        cout<<"Adult"<<endl;
+    } else {
+        cout<<"Teenager"<<endl;
+    }
+    return 0;
+}

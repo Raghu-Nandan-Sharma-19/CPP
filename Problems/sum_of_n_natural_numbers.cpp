da088776dcef6accf93048be3f7e63cf4f8b8091
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-30 16:29:10 +0530

    Sum of first n natural numbers

---
+++ Problems/sum_of_n_natural_numbers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int num;
+    cout<<"Enter number : ";
+    cin>>num;
+    int sum = (num*(num+1))/2;
+    cout<<"Sum of first "<<num<<" natural numbers is : "<<sum<<endl;
+    return 0;
+}

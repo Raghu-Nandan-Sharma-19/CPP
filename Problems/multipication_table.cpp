85f83bad2dc79781598c8db204388c66dd62ef6f
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-30 16:01:16 +0530

    Multiplication table

---
+++ Problems/multipication_table.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int count=1; count<=10; count++) {
+        cout<<n<<" * "<<count<<" = "<<n*count<<endl;
+    }
+    return 0;
+}

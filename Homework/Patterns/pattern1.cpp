334c169bdd318732d15c283b7c94428ba5655fe8
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-01 18:30:57 +0530

    Homework for pattern part 1

---
+++ Homework/Patterns/pattern1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int row=1; row<=5; row++) {
+        for(int col=1; col<=5; col++) {
+            cout<<"4"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Homework/Patterns/pattern4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int row=1; row<=5; row++) {
+        for(char col='F'; col<='K'; col++) {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

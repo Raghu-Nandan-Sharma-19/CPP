d022b109126d869acb6ee368cddc727069121bcc
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-01 18:22:11 +0530

    Added alternate code for number pattern 4

--- Patterns/number_pattern_4.cpp
+++ Patterns/number_pattern_4.cpp
@@ -1,10 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int count = 1;
+    // int count = 1;
+    // for(int row=1; row<=5; row++) {
+    //     for(int col=1; col<=5; col++) {
+    //         cout<<count++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // alternate way
     for(int row=1; row<=5; row++) {
         for(int col=1; col<=5; col++) {
-            cout<<count++<<" ";
+            cout<<(row-1)*5+col<<" ";
         }
         cout<<endl;
     }

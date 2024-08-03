45c8031abb90708e461d6a1b8d7704a7924fcfe6
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-03 23:27:34 +0530

    Added comments to hard pattern 4

--- Patterns/HardPatterns/pattern_4.cpp
+++ Patterns/HardPatterns/pattern_4.cpp
@@ -5,9 +5,11 @@ int main() {
     cout<<"Enter number : ";
     cin>>n;
     for(int row=1; row<=n; row++) {
+        // space print
         for(int col=1; col<=n-row; col++) {
             cout<<"  ";
         }
+        // alphabet print
         for(char col='A'; col<='A'+row-1; col++) {
             cout<<col<<" ";
         }

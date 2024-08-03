562ea3e0e8f87950025aad16e5c3d99032041c44
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-03 20:05:33 +0530

    Hard pattern 2

---
+++ Patterns/HardPatterns/pattern_2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int row=1; row<=n; row++) {
+        // space print
+        for(int col=1; col<=n-row; col++) {
+            cout<<"  ";
+        }
+        // number print
+        for(int col=1; col<=row; col++) {
+            cout<<row<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

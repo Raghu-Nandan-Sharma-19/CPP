30be0eedc25a0b5b27257fd4fb35a38edd1c3ef9
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-05 18:32:31 +0530

    Added comments to advanced pattern 2

--- Patterns/AdvancedPatterns/pattern_2.cpp
+++ Patterns/AdvancedPatterns/pattern_2.cpp
@@ -5,12 +5,15 @@ int main() {
     cout<<"Enter number : ";
     cin>>n;
     for(int row=1; row<=n; row++) {
+        // space print
         for(int col=1; col<=n-row; col++) {
             cout<<"  ";
         }
+        // 1 to row print
         for(int col=1; col<=row; col++) {
             cout<<col<<" ";
         }
+        // row-1 to 1 print
         for(int col=row-1; col>=1; col--) {
             cout<<col<<" ";
         }

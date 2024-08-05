844ad84d51c63334f83e19debea938b8584e7e5c
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-06 00:32:37 +0530

    Advanced pattern 6

---
+++ Patterns/AdvancedPatterns/pattern_6.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter number : ";
+    cin>>n;
+    for(int row=1; row<=n; row++) {
+        for(int col=1; col<=n-row; col++) {
+            cout<<" ";
+        }
+        for(int col=1; col<=row; col++) {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    for(int row=5; row>=1; row--) {
+        for(int col=1; col<=n-row; col++) {
+            cout<<" ";
+        }
+        for(int col=1; col<=row; col++) {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

8edbed31397ea0d8334b0af2b65af970713db915
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-06 16:54:57 +0530

    Unary operators

---
+++ Operators/unary_operators.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int num = 10;
+
+    // post increment
+    cout<<num++<<endl;
+    cout<<num<<endl;
+    
+    // pre increment
+    cout<<++num<<endl;
+    cout<<num<<endl;
+
+    // post decrement
+    cout<<num--<<endl;
+    cout<<num<<endl;
+
+    // pre decrement
+    cout<<--num<<endl;
+    cout<<num<<endl;
+
+    return 0;
+}

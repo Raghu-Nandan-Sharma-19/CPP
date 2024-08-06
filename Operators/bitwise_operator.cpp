1c609fed5a5a6f7e522c6cb5e442dca748f127ea
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-07 01:43:45 +0530

    Bitwise operators

---
+++ Operators/bitwise_operator.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    // & operator
+    int ans = 2&3;
+    cout<<ans<<endl;
+    // | operator
+    ans = 2|3;
+    cout<<ans<<endl;
+    // ^ operator
+    ans = 2^3;
+    cout<<ans<<endl;
+    // << operator
+    ans = 2<<3;
+    cout<<ans<<endl;
+    // >> operator
+    ans = 2>>1;
+    cout<<ans<<endl;
+    // ~ operator
+    ans = ~5;
+    cout<<ans<<endl;
+}

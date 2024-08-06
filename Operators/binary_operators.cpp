cdaa7421c3a62533127a5489736922a1be4678cd
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-06 16:50:20 +0530

    Binary operators

---
+++ Operators/binary_operators.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int num1 = 5;
+    int num2 = 2;
+    // addition
+    cout<<num1+num2<<endl;
+    // subtraction
+    cout<<num1-num2<<endl;
+    // multiplication
+    cout<<num1*num2<<endl;
+    // division
+    cout<<num1/num2<<endl;
+    // modulus
+    cout<<num1%num2<<endl;
+    return 0;
+}

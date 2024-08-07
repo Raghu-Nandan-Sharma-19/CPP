ddc4532c803e6e7be96f2778267d7480bfb0d938
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-08-08 00:12:33 +0530

    Switch case

---
+++ switch.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int number;
+    cout<<"Enter the number : ";
+    cin>>number;
+    switch (number)
+    {
+        case 1:
+        cout<<"Rohit"<<endl;
+        break;
+
+        case 2:
+        cout<<"Mohit"<<endl;
+        break;
+        
+        default:
+        cout<<"Sohit"<<endl;
+    }
+    return 0;
+}
